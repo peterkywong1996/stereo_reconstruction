Fix epilines drawing for 80point plots Add an option to reject RANSAC model with the average error of the manaul 8 points

main.py
@@ -159,13 +159,13 @@ class RansacModel(object):
         # return error per point
         return err
 
-def F_from_ransac(x1, x2, model, n, k, t, d, debug=False, return_all=False):
+def F_from_ransac(x1, x2, model, n, k, t, d, debug=False, return_all=False, avg_err=None):
     import ransac
     
     data = np.vstack((x1,x2))
     
     # compute F and return with inlier index
-    F, ransac_data = ransac.ransac(data.T, model, n, k, t, d, debug, return_all)
+    F, ransac_data = ransac.ransac(data.T, model, n, k, t, d, debug, return_all, avg_err)
     
     return F, ransac_data['inliers']
 
@@ -199,6 +199,15 @@ def drawEpilines(img1,img2,pts1,pts2,F):
 
     return imgLeft, imgRight
 
+def findAverageError(pts1, pts2):
+    model = RansacModel(debug=False, return_all=True)
+    data = np.vstack((pts1, pts2))
+
+    maybemodel = model.fit(data.T)
+    print(model.get_error(data.T, maybemodel))
+    r = np.mean(model.get_error(data.T, maybemodel))
+    print(r)
+    return r
 
 def main():    
     parser = argparse.ArgumentParser()
@@ -217,6 +226,8 @@ def main():
     parser.add_argument('--min_close_data', type=int)    
     parser.add_argument('--max_iteration', type=int)
     parser.add_argument('--max_error', type=float)
+    parser.add_argument('--use_manual_baseline', action='store_true', default=False,
+                        help='Impose RANSAC rejection by the average error found with 8 manual points')
     args = parser.parse_args()
     
     if args.manual:
@@ -237,9 +248,16 @@ def main():
         k = args.max_iteration
         t = args.max_error
         d = args.min_close_data
+
+        avg_8pts_err = None
+        if args.use_manual_baseline:
+            x1_manual = load_points(args.dir_x1)
+            x2_manual = load_points(args.dir_x2)
+            avg_8pts_err = findAverageError(x1_manual, x2_manual)
         
         model = RansacModel(debug=False, return_all=True)
-        F_ransac, data_ransac = F_from_ransac(x1, x2, model, n, k, t, d, debug=model.debug, return_all=model.return_all)
+        F_ransac, data_ransac = F_from_ransac(x1, x2, model, n, k, t, d, debug=model.debug, return_all=model.return_all, avg_err=avg_8pts_err)
+    
     
     N_in = len(data_ransac)
     print('\nNumber of inliers: ', N_in)
@@ -296,23 +314,34 @@ def main():
     img1 = cv2.imread(args.dir_img1)
     img2 = cv2.imread(args.dir_img2)
 
-    imgCvBest8Left, imgCvBest8Right = drawEpilines(img1, img2, x1.T, x2.T, F_cv_best8)
+    imgCvBest8Left, imgCvBest8Right = drawEpilines(img1, img2, x1_best8.T, x2_best8.T, F_cv_best8)
     imgCvSiftLeft, imgCvSiftRight = drawEpilines(img1, img2, x1_inliers.T, x2_inliers.T, F_cv_sift)
 
-    imgNormBest8Left, imgNormBest8Right = drawEpilines(img1, img2, x1.T, x2.T, F_norm_best8)
+    imgNormBest8Left, imgNormBest8Right = drawEpilines(img1, img2, x1_best8.T, x2_best8.T, F_norm_best8)
     imgNormSiftLeft, imgNormSiftRight = drawEpilines(img1, img2, x1_inliers.T, x2_inliers.T, F_norm_sift)
 
-    imgUnnormBest8Left, imgUnnormBest8Right = drawEpilines(img1, img2, x1.T, x2.T, F_unnorm_best8)
+    imgUnnormBest8Left, imgUnnormBest8Right = drawEpilines(img1, img2, x1_best8.T, x2_best8.T, F_unnorm_best8)
     imgUnnormSiftLeft, imgUnnormSiftRight = drawEpilines(img1, img2, x1_inliers.T, x2_inliers.T, F_unnorm_sift)
 
-    plt.subplot(2,2,1),plt.imshow(imgCvSiftLeft)
-    plt.subplot(2,2,2),plt.imshow(imgCvSiftRight)
+    f1= plt.figure(1)
+    plt.subplot(2,2,1),plt.imshow(imgCvBest8Left)
+    plt.subplot(2,2,2),plt.imshow(imgCvBest8Right)
+
+    plt.subplot(2,2,3),plt.imshow(imgCvSiftLeft)
+    plt.subplot(2,2,4),plt.imshow(imgCvSiftRight)
+    f1.show()
+
+    f2 = plt.figure(2)
+    plt.subplot(2,2,1),plt.imshow(imgNormBest8Left)
+    plt.subplot(2,2,2),plt.imshow(imgNormBest8Right)
 
     plt.subplot(2,2,3),plt.imshow(imgNormSiftLeft)
     plt.subplot(2,2,4),plt.imshow(imgNormSiftRight)
-    plt.show()
+    f2.show()
+    
+    plt.waitforbuttonpress(0)
 
-    print("imgCvSiftLeft/imgNormSiftLeft? {}".format(
+    print("imgCvSiftLeft/imgNormSiftLeft different? {}".format(
         np.count_nonzero(imgCvSiftLeft != imgNormSiftLeft) > 0
     ))
     

ransac.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy import dot
 from scipy.linalg import lstsq
 
-def ransac(data,model,n,k,t,d,debug=False,return_all=False):
+def ransac(data,model,n,k,t,d,debug=False,return_all=False, avg_err=None):
     """
     Given:
         data - a set of observed data points
@@ -11,6 +11,7 @@ def ransac(data,model,n,k,t,d,debug=False,return_all=False):
         k - the maximum number of iterations allowed in the algorithm
         t - a threshold value for determining when a data point fits a model
         d - the number of close data values required to assert that a model fits well to data
+        avg_err - the average error that a model is rejected if it passes. None means no such rejection.
     Return:
         bestfit - model parameters which best fit the data (or nil if no good model is found)
     iterations = 0
@@ -62,7 +63,7 @@ def ransac(data,model,n,k,t,d,debug=False,return_all=False):
             bettermodel = model.fit(betterdata)
             better_errs = model.get_error( betterdata, bettermodel)
             thiserr = np.mean( better_errs )
-            if thiserr < besterr:
+            if (avg_err is None or thiserr <= avg_err) and thiserr < besterr:
                 bestfit = bettermodel
                 besterr = thiserr
                 best_inlier_idxs = np.concatenate( (maybe_idxs, also_idxs) )